d6cffd3cfecd3a7075c123c03490cd137c115e7f
mszhang <mszhang02@gmail.com>
2020-01-14 01:40:06 -0500

    Updates

---
+++ precache-manifest.58358be39b76fc33448cf898530333d2.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5620899b98f162d53c934267f87e86",
+    "url": "/poke-clicker-1/index.html"
+  },
+  {
+    "revision": "7792d2dc07bab95efa1a",
+    "url": "/poke-clicker-1/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "39150e2135c3133fbb60",
+    "url": "/poke-clicker-1/static/css/main.a3a48ca0.chunk.css"
+  },
+  {
+    "revision": "7792d2dc07bab95efa1a",
+    "url": "/poke-clicker-1/static/js/2.d426aadc.chunk.js"
+  },
+  {
+    "revision": "44d23042166cd0438bdfd88e10bbc745",
+    "url": "/poke-clicker-1/static/js/2.d426aadc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39150e2135c3133fbb60",
+    "url": "/poke-clicker-1/static/js/main.cb20665e.chunk.js"
+  },
+  {
+    "revision": "7621a5e4628ee654ccfc",
+    "url": "/poke-clicker-1/static/js/runtime-main.f6672dea.js"
+  },
+  {
+    "revision": "2c0b094e3e28b5c882dc9af78612bec8",
+    "url": "/poke-clicker-1/static/media/494.2c0b094e.gif"
+  },
+  {
+    "revision": "20e31d9e26c1417a370be32807d30d19",
+    "url": "/poke-clicker-1/static/media/495.20e31d9e.gif"
+  },
+  {
+    "revision": "aad7edaaaf19c363f2d583ab967aaf1d",
+    "url": "/poke-clicker-1/static/media/496.aad7edaa.gif"
+  },
+  {
+    "revision": "b63accda17a8a99ed1e08f1479050207",
+    "url": "/poke-clicker-1/static/media/497.b63accda.gif"
+  },
+  {
+    "revision": "ac3600478259ca8e04bbad757129d57a",
+    "url": "/poke-clicker-1/static/media/498.ac360047.gif"
+  },
+  {
+    "revision": "04b55bbe7f4ed4e4621023c5801b1bfd",
+    "url": "/poke-clicker-1/static/media/499.04b55bbe.gif"
+  },
+  {
+    "revision": "36eb6f730fc874aa8b1486deab15d476",
+    "url": "/poke-clicker-1/static/media/500.36eb6f73.gif"
+  },
+  {
+    "revision": "bf16a9d5c7d07b1644778a340d520378",
+    "url": "/poke-clicker-1/static/media/501.bf16a9d5.gif"
+  },
+  {
+    "revision": "6fca211631b57750cc692bd464d5da8e",
+    "url": "/poke-clicker-1/static/media/502.6fca2116.gif"
+  },
+  {
+    "revision": "e40120b1b8ce10b8d750e029bb4cc08f",
+    "url": "/poke-clicker-1/static/media/503.e40120b1.gif"
+  },
+  {
+    "revision": "755e5f724dc3aaa60160e1cf021d8c10",
+    "url": "/poke-clicker-1/static/media/599.755e5f72.gif"
+  },
+  {
+    "revision": "62918f4a248b99a2e8585d2673263dd8",
+    "url": "/poke-clicker-1/static/media/600.62918f4a.gif"
+  },
+  {
+    "revision": "b1f2c0abd0e4379bfe8836839af8d343",
+    "url": "/poke-clicker-1/static/media/601.b1f2c0ab.gif"
+  },
+  {
+    "revision": "34ac22bc3780fa304d672f6658f6e84f",
+    "url": "/poke-clicker-1/static/media/607.34ac22bc.gif"
+  },
+  {
+    "revision": "a64849e4c38005f2f39cf68861b317a4",
+    "url": "/poke-clicker-1/static/media/608.a64849e4.gif"
+  },
+  {
+    "revision": "fda32bae6c1a00e43a6a477b0acb3ae5",
+    "url": "/poke-clicker-1/static/media/609.fda32bae.gif"
+  },
+  {
+    "revision": "6484a5fc8a17240a5f8e658d20ad0d37",
+    "url": "/poke-clicker-1/static/media/627.6484a5fc.gif"
+  },
+  {
+    "revision": "c67f11b2036e2f7b78c9f2c8fa1ec459",
+    "url": "/poke-clicker-1/static/media/628.c67f11b2.gif"
+  },
+  {
+    "revision": "a448fb3c93a0ca045e69b464edb0dce0",
+    "url": "/poke-clicker-1/static/media/629.a448fb3c.gif"
+  },
+  {
+    "revision": "95210138dd56a0e21417c4ac2a6c8606",
+    "url": "/poke-clicker-1/static/media/630.95210138.gif"
+  },
+  {
+    "revision": "ea4bb310fe9720f023c5e7ca5ee486a7",
+    "url": "/poke-clicker-1/static/media/633.ea4bb310.gif"
+  },
+  {
+    "revision": "9d11e63691f9bccafa66e056dd77e064",
+    "url": "/poke-clicker-1/static/media/634.9d11e636.gif"
+  },
+  {
+    "revision": "dc44152567208f1570154e86c91ba58a",
+    "url": "/poke-clicker-1/static/media/635.dc441525.gif"
+  },
+  {
+    "revision": "65eb1809c2e01074cb8f7e2b3789ef01",
+    "url": "/poke-clicker-1/static/media/636.65eb1809.gif"
+  },
+  {
+    "revision": "33aef799c868f3b9c0dae73467472b85",
+    "url": "/poke-clicker-1/static/media/637.33aef799.gif"
+  },
+  {
+    "revision": "81fcc669199ba3d12374329e5a58dcb6",
+    "url": "/poke-clicker-1/static/media/641.81fcc669.gif"
+  },
+  {
+    "revision": "b3dbe12af34824c09e392a02d79cfee6",
+    "url": "/poke-clicker-1/static/media/642.b3dbe12a.gif"
+  },
+  {
+    "revision": "e2fe77111b854e5800d569bd5114e4fe",
+    "url": "/poke-clicker-1/static/media/643.e2fe7711.gif"
+  },
+  {
+    "revision": "48c6d0de8581abad9f80ef02a3ea2e2e",
+    "url": "/poke-clicker-1/static/media/644.48c6d0de.gif"
+  },
+  {
+    "revision": "ff2b8ddc8b44a0a685de5ae4bc9d1640",
+    "url": "/poke-clicker-1/static/media/646.ff2b8ddc.gif"
+  },
+  {
+    "revision": "ef653088b50ff240a7b04fc76c1d317e",
+    "url": "/poke-clicker-1/static/media/rarecandy.ef653088.png"
+  }
+]);
